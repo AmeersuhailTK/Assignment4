650bc2afbecb8d2919968f57c07270636287d372
Disney <disneywelcome4062@gmail.com>
2026-05-16 08:26:58 +0300

    16-05  third

--- src/wrapper/Mian.java
+++ src/wrapper/Mian.java
@@ -4,8 +4,17 @@ public class Mian {
 	
 	    public static void main(String[] args) {
 
-	        int num = 50;
-
+	    	int num = 1000;
+	    	
+	    	System.out.println("THeory of WRAPPER CLASS");
+	        System.out.println("------------------------");
+	        System.out.println("Wrapper classes in Java are object representations of primitive data types");
+	        System.out.println("EXAMPLES");
+	        System.out.println("int    >> Integer");
+	        System.out.println("double >> Double");
+	        System.out.println("char   >> Charector");
+	        System.out.println("-------000------------");
+	        
 	        // Primitive to Wrapper (Boxing)
 	        Integer obj = Integer.valueOf(num);
 
@@ -14,9 +23,11 @@ public class Mian {
 
 	        System.out.println("Primitive int: " + num);
 	        System.out.println("Integer object: " + obj);
-	        System.out.println("Back to primitive: " + value);
+	        System.out.println("Back to primitive: " + value);System.out.println("Back to primitive: " + value);
+	        
+	        
 	    }
 	}
 
-
+     
 
